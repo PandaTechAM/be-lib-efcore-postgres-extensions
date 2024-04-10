740758118c12c2731335b4686bbf1b002bd2e675
Haik <haik.asatran@pandatech.it>
2024-04-10 18:22:18 +0400

    npgsqlcopy add

---
+++ test/PandaNuGet.Demo/Context/DatabaseExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PandaNuGet.Demo.Context;
+
+public static class DatabaseExtensions
+{
+    public static WebApplicationBuilder AddPostgresContext(this WebApplicationBuilder builder)
+    {
+        var configuration = builder.Configuration;
+
+        var connectionString = configuration.GetConnectionString("Postgres");
+        builder.Services.AddDbContextPool<PostgresContext>(options =>
+            options.UseNpgsql(connectionString));
+        return builder;
+    }
+    
+    public static WebApplication ResetDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<PostgresContext>();
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+        return app;
+    }
+
+    public static WebApplication MigrateDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<PostgresContext>();
+        dbContext.Database.Migrate();
+        return app;
+    }
+}
---
+++ test/PandaNuGet.Demo/Context/PostgresContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PandaNuGet.Demo.Entities;
+
+namespace PandaNuGet.Demo.Context;
+
+public class PostgresContext(DbContextOptions<PostgresContext> options) : DbContext(options)
+{
+    public DbSet<UserEntity> Users { get; set; } = null!;
+}
