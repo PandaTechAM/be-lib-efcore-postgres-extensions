f6e96b870b7d448ea98668afd02da211133e053e
MnatsakanMargaryan <MnatsakanMargaryan@MNATS>
2026-06-19 16:11:11 +0400

    Public documentation for methods added.

--- src/EFCore.PostgresExtensions/Extensions/DbSetExtensions.cs
+++ src/EFCore.PostgresExtensions/Extensions/DbSetExtensions.cs
@@ -3,8 +3,17 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace EFCore.PostgresExtensions.Extensions;
 
+/// <summary>
+///    Provides internal helpers for retrieving EF Core infrastructure from a DbSet.
+/// </summary>
 internal static class DbSetExtensions
 {
+   /// <summary>
+   ///    Gets the DbContext instance that owns the specified DbSet.
+   /// </summary>
+   /// <typeparam name="T">The entity type contained in the DbSet.</typeparam>
+   /// <param name="dbSet">The DbSet whose owning DbContext should be returned.</param>
+   /// <returns>The DbContext instance associated with the DbSet.</returns>
    public static DbContext GetDbContext<T>(this DbSet<T> dbSet) where T : class
    {
       IInfrastructure<IServiceProvider> infrastructure = dbSet;
--- src/EFCore.PostgresExtensions/Extensions/OptionsBuilderExtensions.cs
+++ src/EFCore.PostgresExtensions/Extensions/OptionsBuilderExtensions.cs
@@ -3,8 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EFCore.PostgresExtensions.Extensions;
 
+/// <summary>
+///    Provides extension methods for configuring PostgreSQL-specific EF Core options.
+/// </summary>
 public static class OptionsBuilderExtensions
 {
+   /// <summary>
+   ///    Registers the query-lock interceptor required by <see cref="QueryableExtensions.ForUpdate{T}"/>.
+   /// </summary>
+   /// <param name="builder">The EF Core context options builder to configure.</param>
+   /// <returns>The same options builder instance for fluent configuration.</returns>
    public static DbContextOptionsBuilder UseQueryLocks(this DbContextOptionsBuilder builder)
    {
       builder.AddInterceptors(new TaggedQueryCommandInterceptor());
